74ab5449b2671538006f76dd6c8f00353e52a4f6
paulo_sneto <paulo_sneto@yahoo.com>
2024-07-12 16:31:29 -0300

    feat: created the default classes

---
+++ src/main/java/com/agenda/book/AgendaBookApplication.java
@@ -0,0 +1,13 @@
+package com.agenda.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaBookApplication.class, args);
+	}
+
+}
