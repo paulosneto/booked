9faadb5d8058ffa98244e5de7bfe55b516aea10c
paulo_sneto <paulo_sneto@yahoo.com>
2024-07-12 16:38:02 -0300

    feat: created the migration sql

---
+++ src/main/resources/db/migration/V1__create_table_paciente.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PACIENTE_TB(
+    idPaciente PRIMARY KEY,
+    nomePaciente VARCHAR(200),
+    cpfPaciente VARCHAR(11),
+    emailPaciente VARCHAR(200),
+    telefonePaciente VARCHAR(20)
+
+)
