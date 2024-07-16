68533ce232406f6090347874650270654471fec7
paulo_sneto <paulo_sneto@yahoo.com>
2024-07-16 16:56:21 -0300

    fix: modified AgendaDTO class

--- src/main/java/com/agenda/book/dtos/AgendaDTO.java
+++ src/main/java/com/agenda/book/dtos/AgendaDTO.java
@@ -1,8 +1,13 @@
 package com.agenda.book.dtos;
 
 import com.agenda.book.domains.Paciente;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.NotFound;
 
 import java.time.LocalDateTime;
 
-public record AgendaDTO(String descricao, LocalDateTime dataHora, LocalDateTime dataCriacao, Paciente paciente) {
+public record AgendaDTO(@NotBlank String descricao,
+                        @NotBlank LocalDateTime dataHoraAgendamento,
+                        //@NotBlank LocalDateTime dataCriacao,
+                        @NotBlank String cpfPaciente) {
 }
