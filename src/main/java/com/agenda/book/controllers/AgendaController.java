c36a248888afb594d357523e6096bb3175b8640d
paulo_sneto <paulo_sneto@yahoo.com>
2024-07-17 16:52:39 -0300

    fix: modified AgendaController class

--- src/main/java/com/agenda/book/controllers/AgendaController.java
+++ src/main/java/com/agenda/book/controllers/AgendaController.java
@@ -4,6 +4,9 @@ import com.agenda.book.domains.Agenda;
 import com.agenda.book.dtos.AgendaDTO;
 import com.agenda.book.repositories.AgendaRepository;
 import com.agenda.book.services.AgendaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +23,8 @@ public class AgendaController {
     @Autowired
     private AgendaService service;
 
+    @Operation(summary = "Created new Schedule(Agendamento)")
+    @ApiResponse(responseCode = "201", description = "Schedule created successfully")
     @PostMapping
     public ResponseEntity<Agenda> createAgenda(@RequestBody AgendaDTO dto){
 
@@ -28,11 +33,18 @@ public class AgendaController {
         return ResponseEntity.ok(agenda);
     }
 
+    @Operation(summary = "Get all Schedules(Agendamentos)", description = "Returned all Schedules")
+    @ApiResponse(responseCode = "200", description = "Schedules found")
     @GetMapping
     public ResponseEntity<List<Agenda>> getAll(){
         return ResponseEntity.ok(this.service.findAll());
     }
 
+    @Operation(summary = "Get schedule(Agendamento) by creation date")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Schedule found"),
+            @ApiResponse(responseCode = "404", description = "Schedule not found")
+    })
     @GetMapping("/{dataCriacao}")
     public ResponseEntity<List<Agenda>> findByDataCriacao(@PathVariable("dataCriacao") LocalDateTime dataCriacao, String cpfPaciente){
        List<Agenda> lt = this.service.findByDataCriacao(dataCriacao);
