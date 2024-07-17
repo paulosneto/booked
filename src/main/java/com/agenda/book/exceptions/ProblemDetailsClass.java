9bc4a01d1f102dd05034fa9c845153dfa2cdf0d5
paulo_sneto <paulo_sneto@yahoo.com>
2024-07-17 16:54:19 -0300

    fix: modified ProblemDetailsClass

--- src/main/java/com/agenda/book/exceptions/ProblemDetailsClass.java
+++ src/main/java/com/agenda/book/exceptions/ProblemDetailsClass.java
@@ -5,10 +5,15 @@ import org.springframework.http.ProblemDetail;
 
 public abstract class ProblemDetailsClass extends RuntimeException{
 
-    public ProblemDetail errorProblemDetail(){
-        var pb = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-        pb.setDetail("Server internal error.");
-        pb.setStatus(500);
+    public ProblemDetail errorProblemDetail(String message, int statusCode){
+        //var pb = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        var pb = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        //pb.setDetail("Server internal error.");
+        //pb.setTitle("Server internal error.");
+        //pb.setStatus(500);
+        pb.setTitle(message);
+        pb.setStatus(statusCode);
 
         return pb;
     }
