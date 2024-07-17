d6d2052e8b13c7e8f5d1cdd37c987c7f89b0542e
paulo_sneto <paulo_sneto@yahoo.com>
2024-07-17 16:53:34 -0300

    fix: modified PacienteController class

--- src/main/java/com/agenda/book/controllers/PacienteController.java
+++ src/main/java/com/agenda/book/controllers/PacienteController.java
@@ -3,6 +3,9 @@ package com.agenda.book.controllers;
 import com.agenda.book.domains.Paciente;
 import com.agenda.book.dtos.PacienteDTO;
 import com.agenda.book.services.PacienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +26,8 @@ public class PacienteController {
         this.service = service;
     }
 
+    @Operation(summary = "Created new Patient(Paciente)")
+    @ApiResponse(responseCode = "201", description = "Patient saved successfully")
     @PostMapping
     public ResponseEntity<Paciente> createPaciente(@RequestBody PacienteDTO dto){
         //Passa a DTO de requisição para a classe service de
@@ -32,22 +37,36 @@ public class PacienteController {
         return ResponseEntity.ok().body(p);
     }
 
+    @Operation(summary = "Get all Patients(Pacientes)")
+    @ApiResponse(responseCode = "200", description = "Patients found")
     @GetMapping
     public ResponseEntity<List<Paciente>> listPacientes(){
         // Retorna a lista com todos os pacientes
         return ResponseEntity.ok().body(this.service.listPaciente());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Paciente> findByIdPaciente(@PathVariable("id") Long id) {
-       var paciente = this.service.findById(id);
+    @Operation(summary = "Find Patient(Paciente) by CPF")
+    @ApiResponses(
+            value = {
+                @ApiResponse(responseCode = "201", description = "Patient found"),
+                @ApiResponse(responseCode = "404", description = "Patient not found")
+            })
+    @GetMapping("/{cpf}")
+    public ResponseEntity<Paciente> findByIdPaciente(@PathVariable("cpf") String cpf) {
+       //var paciente = this.service.findById(id);
+        var paciente = this.service.findByCpfPaciente(cpf);
 
        return ResponseEntity.ok().body(paciente);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Paciente> delete(@PathVariable("id") Long id){
-        this.service.deletePaciente(id);
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Remove Patient(Paciente) by CPF"),
+        @ApiResponse(responseCode = "404", description = "CPF the Patient(Paciente) not found")
+    })
+    @DeleteMapping("/{cpf}")
+    public ResponseEntity<Paciente> delete(@PathVariable("cpf") String cpf){
+        //this.service.deletePaciente(id);
+        this.service.deletePacientePorCpf(cpf);
         return ResponseEntity.noContent().build();
 
 
