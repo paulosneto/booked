baa25238a09fc39f42243d91c44d4152236f1d58
paulo_sneto <paulo_sneto@yahoo.com>
2024-07-19 17:04:28 -0300

    fix: modified ExceptionCustomError

--- src/main/java/com/agenda/book/exceptions/ExceptionCustomError.java
+++ src/main/java/com/agenda/book/exceptions/ExceptionCustomError.java
@@ -8,14 +8,17 @@ public class ExceptionCustomError extends ProblemDetailsClass  {
     private final int status;
 
     public ExceptionCustomError(String detail, int status) {
+        super(detail,status);
         this.detail = detail;
         this.status = status;
     }
 
-    @Override
+    /*@Override
     public ProblemDetail errorProblemDetail(String detail, int status) {
         var pd = ProblemDetail.forStatus(this.status);
+        pd.setTitle("ERROR");
         pd.setDetail(this.detail);
+
         return pd;
-    }
+    }*/
 }
