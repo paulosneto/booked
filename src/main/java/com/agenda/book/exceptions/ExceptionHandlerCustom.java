ee30ed9083bcd4d18ffeaff86b2e1b0a90c7aef2
paulo_sneto <paulo_sneto@yahoo.com>
2024-07-19 17:04:43 -0300

    fix: modified ExceptionHandlerCustom

--- src/main/java/com/agenda/book/exceptions/ExceptionHandlerCustom.java
+++ src/main/java/com/agenda/book/exceptions/ExceptionHandlerCustom.java
@@ -2,13 +2,25 @@ package com.agenda.book.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 // Classe criada para lançar a requisição um codigo de erro tratado
 @RestControllerAdvice
-public class ExceptionHandlerCustom extends Throwable {
+public class ExceptionHandlerCustom extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ProblemDetailsClass.class)
+    public ProblemDetail falha(ProblemDetailsClass e){
+        ProblemDetail pb = ProblemDetail.forStatus(e.status);
+        pb.setDetail(e.message);
+
+        /*ExceptionDTO dto = new ExceptionDTO(message, "403");
+        return ResponseEntity.ok(dto);*/
+        return pb;
+    }
 
     // Exceção usada para retornar mensagem caso o
     // objeto a ser procurado no banco ja exista
